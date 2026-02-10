6d0ff036abdc6335f0f306cced551ceb437382a7
Hark Dhami <codeharish07@gmail.com>
2026-02-10 19:04:38 +0545

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='container mx-auto'>
+      <div>
+        
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
